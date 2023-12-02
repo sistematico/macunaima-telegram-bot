14c813d2f5146f6b76b2ae53350cf0a76a20921a
Lucas Saliés Brum <sistematico@gmail.com>
2023-12-02 05:08:30 -0400

    Update

--- scripts/db/create.sh
+++ scripts/db/create.sh
@@ -1,22 +1,13 @@
 #!/bin/bash
 
-DB_CONNECTION_STRING=$(grep 'DATABASE_URL' .env | cut -d '=' -f 2-)
+DB_CONNECTION_STRING=$(grep 'DATABASE_URL' .env | cut -d '=' -f 2- | tr -d '"')
 
 if [ ! -z "$DB_CONNECTION_STRING" ]; then
-    # Removendo 'postgresql://' e tudo após o '@'
     DB_USER=$(echo $DB_CONNECTION_STRING | sed -e 's/postgresql:\/\///' | sed -e 's/:.*//')
-
-    # Removendo tudo até e incluindo ':', e então tudo após o '@'
     DB_PASS=$(echo $DB_CONNECTION_STRING | sed -e 's/.*://' | sed -e 's/@.*//')
-
-    # Removendo tudo até e incluindo '@', e então tudo após o ':'
     DB_HOST=$(echo $DB_CONNECTION_STRING | sed -e 's/.*@//' | sed -e 's/:.*//')
-
-    # Pegando a porta
     DB_PORT=$(echo $DB_CONNECTION_STRING | sed -e 's/.*://' | sed -e 's/\/.*//')
-
-    # Pegando o nome do banco de dados
-    DB_NAME=$(echo $DB_CONNECTION_STRING | sed -e 's/.*\///')
+    DB_NAME=$(echo $DB_CONNECTION_STRING | sed -e 's/.*\///' | sed -e 's/\?.*//')
 fi
 
 does_db_exist() { 
