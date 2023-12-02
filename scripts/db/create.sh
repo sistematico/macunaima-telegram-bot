57e579110dde7a954bb7081b036b0a1e848f35de
Lucas Saliés Brum <sistematico@gmail.com>
2023-12-02 05:33:16 -0400

    Update

--- scripts/db/create.sh
+++ scripts/db/create.sh
@@ -1,24 +1,22 @@
 #!/bin/bash
 
-#DB_CONNECTION_STRING=$(grep 'DATABASE_URL' .env | cut -d '=' -f 2- | tr -d '"')
+# DB_CONNECTION_STRING=$(grep 'DATABASE_URL' .env | cut -d '=' -f 2- | tr -d '"')
 
-#if [ ! -z "$DB_CONNECTION_STRING" ]; then
-#    DB_USER=$(echo $DB_CONNECTION_STRING | sed -e 's/postgresql:\/\///' | sed -e 's/:.*//')
-#    DB_PASS=$(echo $DB_CONNECTION_STRING | sed -e 's/.*://' | sed -e 's/@.*//')
-#    DB_HOST=$(echo $DB_CONNECTION_STRING | sed -e 's/.*@//' | sed -e 's/:.*//')
-#    DB_PORT=$(echo $DB_CONNECTION_STRING | sed -e 's/.*://' | sed -e 's/\/.*//')
-#    DB_NAME=$(echo $DB_CONNECTION_STRING | sed -e 's/.*\///' | sed -e 's/\?.*//')
-#fi
+# if [ ! -z "$DB_CONNECTION_STRING" ]; then
+#     DB_USER=$(echo $DB_CONNECTION_STRING | sed -e 's/postgresql:\/\///' | sed -e 's/:.*//')
+#     DB_PASS=$(echo $DB_CONNECTION_STRING | sed -e 's/.*://' | sed -e 's/@.*//')
+#     DB_HOST=$(echo $DB_CONNECTION_STRING | sed -e 's/.*@//' | sed -e 's/:.*//')
+#     DB_PORT=$(echo $DB_CONNECTION_STRING | sed -e 's/.*://' | sed -e 's/\/.*//')
+#     DB_NAME=$(echo $DB_CONNECTION_STRING | sed -e 's/.*\///' | sed -e 's/\?.*//')
 
 DB_CONNECTION_STRING=$(grep 'DATABASE_URL' .env | cut -d '=' -f 2- | tr -d '"')
 
 if [ ! -z "$DB_CONNECTION_STRING" ]; then
-    DB_USER=$(echo $DB_CONNECTION_STRING | sed -e 's/postgresql:\/\///' | sed -e 's/:.*//')
-    DB_PASS=$(echo $DB_CONNECTION_STRING | sed -e 's/.*://' | sed -e 's/@.*//')
-    DB_HOST=$(echo $DB_CONNECTION_STRING | sed -e 's/.*@//' | sed -e 's/:.*//')
-    DB_PORT=$(echo $DB_CONNECTION_STRING | sed -e 's/.*://' | sed -e 's/\/.*//')
-    DB_NAME=$(echo $DB_CONNECTION_STRING | sed -e 's/.*\///' | sed -e 's/\?.*//')
-
+    DB_USER=$(echo $DB_CONNECTION_STRING | sed -e 's/postgresql:\/\///' | cut -d ':' -f 1)
+    DB_PASS=$(echo $DB_CONNECTION_STRING | cut -d ':' -f 2 | cut -d '@' -f 1)
+    DB_HOST=$(echo $DB_CONNECTION_STRING | cut -d '@' -f 2 | cut -d ':' -f 1)
+    DB_PORT=$(echo $DB_CONNECTION_STRING | cut -d ':' -f 3 | cut -d '/' -f 1)
+    DB_NAME=$(echo $DB_CONNECTION_STRING | cut -d '/' -f 3 | cut -d '?' -f 1)
     echo "Usuário: $DB_USER"
     echo "Senha:   $DB_PASS"
     echo "Host:    $DB_HOST"
