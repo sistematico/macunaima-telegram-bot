56fade0ecc09476c44a59746d89443102df9c0b5
Lucas Saliés Brum <sistematico@gmail.com>
2023-12-02 05:05:09 -0400

    Update

--- scripts/db/create.sh
+++ scripts/db/create.sh
@@ -1,39 +1,31 @@
 #!/bin/bash
 
-
-#DB_CONNECTION_STRING=$(grep 'DATABASE_URL' .env | cut -d '=' -f 2-)
-# DB_CONNECTION_STRING=$(grep "DATABASE_URL" $ENV_FILE)
-
-# if [ ! -z "$DB_CONNECTION_STRING" ]; then
-#     DB_CREDENTIALS=$(echo $DB_CONNECTION_STRING | sed -e 's/DATABASE_URL="postgresql:\/\/\(.*\)"/\1/')
-#     DB_USER=$(echo $DB_CREDENTIALS | cut -d ':' -f 1)
-#     DB_PASS=$(echo $DB_CREDENTIALS | cut -d ':' -f 2 | cut -d '@' -f 1)
-#     DB_HOST=$(echo $DB_CREDENTIALS | cut -d '@' -f 2 | cut -d ':' -f 1)
-#     DB_PORT=$(echo $DB_CREDENTIALS | cut -d ':' -f 3 | cut -d '/' -f 1)
-#     DB_NAME=$(echo $DB_CREDENTIALS | cut -d '/' -f 2 | cut -d '?' -f 1)
-# fi
-
 DB_CONNECTION_STRING=$(grep 'DATABASE_URL' .env | cut -d '=' -f 2-)
 
 if [ ! -z "$DB_CONNECTION_STRING" ]; then
-    DB_USER=$(echo $DB_CONNECTION_STRING | sed -e 's,^postgresql://\([^:]*\):.*$,\1,')
-    DB_PASS=$(echo $DB_CONNECTION_STRING | sed -e 's,^postgresql://[^:]*:\([^@]*\)@.*$,\1,')
-    DB_HOST=$(echo $DB_CONNECTION_STRING | sed -e 's,^postgresql://[^@]*@\([^:/]*\).*,\1,')
-    DB_PORT=$(echo $DB_CONNECTION_STRING | sed -e 's,^postgresql://[^@]*@[^:/]*:\([^/]*\)/.*,\1,')
-    DB_NAME=$(echo $DB_CONNECTION_STRING | sed -e 's,^postgresql://[^@]*@[^:/]*:[^/]*\/\(.*\)$,\1,')
+    # Removendo 'postgresql://' e tudo após o '@'
+    DB_USER=$(echo $DB_CONNECTION_STRING | sed -e 's/postgresql:\/\///' | sed -e 's/:.*//')
+
+    # Removendo tudo até e incluindo ':', e então tudo após o '@'
+    DB_PASS=$(echo $DB_CONNECTION_STRING | sed -e 's/.*://' | sed -e 's/@.*//')
+
+    # Removendo tudo até e incluindo '@', e então tudo após o ':'
+    DB_HOST=$(echo $DB_CONNECTION_STRING | sed -e 's/.*@//' | sed -e 's/:.*//')
+
+    # Pegando a porta
+    DB_PORT=$(echo $DB_CONNECTION_STRING | sed -e 's/.*://' | sed -e 's/\/.*//')
+
+    # Pegando o nome do banco de dados
+    DB_NAME=$(echo $DB_CONNECTION_STRING | sed -e 's/.*\///')
 fi
 
-# Função para verificar se um banco de dados existe
-does_db_exist() {
-    su - postgres -c "psql -lqt | cut -d \| -f 1 | grep -qw $1"
-}
+does_db_exist() { su - postgres -c "psql -lqt | cut -d \| -f 1 | grep -qw $1" }
 
 if does_db_exist $DB_NAME; then
     echo "O banco de dados $DB_NAME já existe. O script não será executado."
     exit 0
 fi
 
-# Criar usuário e banco de dados
 su - postgres -c "psql -c \"CREATE USER $DB_USER WITH PASSWORD '$DB_PASS';\""
 su - postgres -c "psql -c \"CREATE DATABASE $DB_NAME WITH OWNER $DB_USER;\""
 
