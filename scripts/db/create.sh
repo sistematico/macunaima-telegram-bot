ec416badf94bf848013e35a6e023e00a5f1318a8
Lucas Saliés Brum <sistematico@gmail.com>
2023-12-02 05:28:55 -0400

    Update

--- scripts/db/create.sh
+++ scripts/db/create.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+#DB_CONNECTION_STRING=$(grep 'DATABASE_URL' .env | cut -d '=' -f 2- | tr -d '"')
+
+#if [ ! -z "$DB_CONNECTION_STRING" ]; then
+#    DB_USER=$(echo $DB_CONNECTION_STRING | sed -e 's/postgresql:\/\///' | sed -e 's/:.*//')
+#    DB_PASS=$(echo $DB_CONNECTION_STRING | sed -e 's/.*://' | sed -e 's/@.*//')
+#    DB_HOST=$(echo $DB_CONNECTION_STRING | sed -e 's/.*@//' | sed -e 's/:.*//')
+#    DB_PORT=$(echo $DB_CONNECTION_STRING | sed -e 's/.*://' | sed -e 's/\/.*//')
+#    DB_NAME=$(echo $DB_CONNECTION_STRING | sed -e 's/.*\///' | sed -e 's/\?.*//')
+#fi
+
 DB_CONNECTION_STRING=$(grep 'DATABASE_URL' .env | cut -d '=' -f 2- | tr -d '"')
 
 if [ ! -z "$DB_CONNECTION_STRING" ]; then
@@ -8,6 +18,12 @@ if [ ! -z "$DB_CONNECTION_STRING" ]; then
     DB_HOST=$(echo $DB_CONNECTION_STRING | sed -e 's/.*@//' | sed -e 's/:.*//')
     DB_PORT=$(echo $DB_CONNECTION_STRING | sed -e 's/.*://' | sed -e 's/\/.*//')
     DB_NAME=$(echo $DB_CONNECTION_STRING | sed -e 's/.*\///' | sed -e 's/\?.*//')
+
+    echo "Usuário: $DB_USER"
+    echo "Senha:   $DB_PASS"
+    echo "Host:    $DB_HOST"
+    echo "Porta:   $DB_PORT"
+    echo "Banco:   $DB_NAME"
 fi
 
 does_db_exist() { 
@@ -19,7 +35,8 @@ if does_db_exist $DB_NAME; then
     exit 0
 fi
 
-su - postgres -c "psql -c \"CREATE USER $DB_USER WITH PASSWORD '$DB_PASS';\""
+su - postgres -c "psql -c \"CREATE USER $DB_USER WITH PASSWORD '$DB_PASS' SUPERUSER;\""
 su - postgres -c "psql -c \"CREATE DATABASE $DB_NAME WITH OWNER $DB_USER;\""
+#su - postgres -c "psql -c \"ALTER USER $DB_USER WITH SUPERUSER;\""
 
 echo "Configuração do banco de dados para Prisma concluída."
