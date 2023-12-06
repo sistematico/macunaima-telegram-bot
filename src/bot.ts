5d1c07f08e1614a5b9570f4abd6d7e671dff3efd
Lucas Saliés Brum <sistematico@gmail.com>
2023-12-06 04:53:27 -0400

    Update

--- src/bot.ts
+++ src/bot.ts
@@ -13,6 +13,22 @@ bot.command(['reportar', 'report', 'warn'], async ctx => warn(ctx))
 bot.on('callback_query:data', async ctx => botCallBack(ctx))
 bot.on('message', async ctx => checkBannedWords(ctx))
 
-app.post('/', webhookCallback(bot, 'hono'))
+// app.post('/', webhookCallback(bot, 'hono'))
+app.get('/', c => c.text('Macunaíma'))
+
+const isProduction = process.env.NODE_ENV === 'production'
+
+if (isProduction) {
+  const webhookUrl = Bun.env.WEBHOOK_URL
+  if (!webhookUrl) throw new Error('WEBHOOK_URL is required for production mode')
+
+  bot.api.setWebhook(webhookUrl)
+  app.post('/', webhookCallback(bot, 'hono'))
+
+  sendLogToChannel('🚀 Bot reiniciado em modo de produção: ' + new Date())
+} else {
+  bot.start()
+  sendLogToChannel('🚀 Bot reiniciado em modo de desenvolvimento: ' + new Date())
+}
 
 export default { port: 3002, fetch: app.fetch }
